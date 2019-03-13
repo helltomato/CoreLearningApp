d51f087f89ac97a3113874fcbc17c1b09b1882de
helltomato <helltomato@gmail.com>
2019-03-13 10:31:23 +0400

    Добавьте файлы проекта.

---
+++ CoreLearningApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreLearningApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
